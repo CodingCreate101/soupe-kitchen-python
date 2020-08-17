cca5bac799c0d4e27b27b442dbffab34483ebb01
Kammar <amrutkumarx.kammar@intel.com>
2020-08-17 21:52:07 +0530

    created templates

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from markupsafe import escape
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Welcome to soup kitchen'
+  return render_template('Recipe_list.html')
+
 
 @app.route('/recipes')
 def reciped_list():
@@ -13,3 +16,7 @@ def reciped_list():
 @app.route('/recipes/<recipe>')
 def selectRecipe(recipe):
   return '{} Recipe'.format(escape(recipe))
+
+@app.route('/<recipe>')
+def hello(recipe=None):
+  return render_template('single_recipe_page.html', recipe=recipe)
